8532ded7d38b1ac61e579300d4c6869022346e5e
Anirudhya <anirudhyaduyti@gmail.com>
2022-04-01 20:07:55 +0600

    chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Charts from './Components/Charts/Charts';
+// import { useState } from 'react';
 
 function App() {
+  // const [chart, setChart]=useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Rechart Axios</h1>
+      <Charts></Charts>
     </div>
   );
 }
