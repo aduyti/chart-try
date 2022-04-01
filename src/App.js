6dedc170d6bc4fe58457902c0a28f36b118a97af
Anirudhya <anirudhyaduyti@gmail.com>
2022-04-01 22:41:23 +0600

    spring render problem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import MyLineCharts from './Components/Charts/MyLineCharts'
 import DataAxios from './Components/DataAxios/DataAxios';
+// import { useSpring, animated } from 'react-spring';
 
 function App() {
+  // const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
   return (
     <div className="App">
+      {/* <animated.div style={props}>I will fade in</animated.div> */}
       <h1>React Rechart Axios</h1>
       <MyLineCharts></MyLineCharts>
       <DataAxios></DataAxios>
